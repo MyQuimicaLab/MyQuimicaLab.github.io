cb20396795a381bf013dedd43853dd79e10aa975
Mei Fagundes <MeiFagundes@outlook.com>
2019-07-29 18:17:46 -0300

    Add MovementController & Change some animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Phaser config
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -16,7 +17,12 @@ var config = {
     pixelArt: true
 };
 
-var game = new Phaser.Game(config);
+// Const
+const playerVelocity = 160;
+
+// Var
+let game = new Phaser.Game(config);
+let movManager, player;
 
 function preload() {
 
@@ -29,9 +35,67 @@ function preload() {
 
 function create() {
 
-    let player = this.physics.add.sprite(50, 50, 'cientista').setScale(3);
+    player = this.physics.add.sprite(50, 50, 'cientista').setScale(3);
+    movManager = new MovementController(player, this.input.keyboard.createCursorKeys(), playerVelocity)
+    setPlayerAnimations();
+
 }
 
 function update() {
 
+    movManager.update();
+
+}
+
+function setPlayerAnimations(){
+
+    game.anims.create({
+        key: 'left',
+        frames: [ { key: 'cientista', frame: 2 } ],
+        frameRate: 10,
+        repeat: -1
+    });
+
+    game.anims.create({
+        key: 'front',
+        frames: [ { key: 'cientista', frame: 0 } ],
+        frameRate: 10
+    });
+
+    game.anims.create({
+        key: 'back',
+        frames: [ { key: 'cientista', frame: 1 } ],
+        frameRate: 10
+    });
+
+    game.anims.create({
+        key: 'right',
+        frames: [{ key: 'cientista', frame: 2 } ],
+        frameRate: 10,
+        repeat: -1
+    });
+
+    game.anims.create({
+        key: 'moving-left',
+        frames: [ { key: 'cientista', frame: 5 }, { key: 'cientista', frame: 6 } ],
+        frameRate: 10
+    });
+
+    game.anims.create({
+        key: 'moving-front',
+        frames: [ { key: 'cientista', frame: 0 }, { key: 'cientista', frame: 3 } ],
+        frameRate: 10
+    });
+
+    game.anims.create({
+        key: 'moving-back',
+        frames: [ { key: 'cientista', frame: 1 }, { key: 'cientista', frame: 4 } ],
+        frameRate: 10
+    });
+
+    game.anims.create({
+        key: 'moving-right',
+        frames: [{ key: 'cientista', frame: 5 }, { key: 'cientista', frame: 6 } ],
+        frameRate: 10
+    });
 }
