e16aef06c8ae86111ecc551cae393ac29a4a99d3
Pedro Paulo <ppagbpedro@gmail.com>
2019-10-08 16:39:40 -0300

    Making QuestionController checks and handle the correct answer
    And change method populateaAlternativesList() to adopt some functional programming style of code.

--- js/Controllers/QuestionModalController.js
+++ js/Controllers/QuestionModalController.js
@@ -11,22 +11,36 @@ class QuestionModalController {
         this._questionTitleEl.innerHTML = `Questão X (${this.multiplier}x)`;
         this._questionDescriptionEl.innerHTML = question.description;
         this._questionImageEl.src = question.imgSrcPath ? question.imgSrcPath : "";
-        this.populateAlternativesList(question.alternatives);
+        this._populateAlternativesList(question.alternatives, question.correctAnswerIndex);
        
     }
 
-    populateAlternativesList(alternatives) {
+    _populateAlternativesList(alternatives, correctAnswerIndex) {
         let currentAlternativeCharacter = "a";
         
-        for(let questionAlternative of alternatives) {
+        alternatives.map((questionAlternative, alternativeIndex) => {
             const alternativeEl = document.createElement('li');
 
             alternativeEl.innerHTML = `${currentAlternativeCharacter.toUpperCase()}) ${questionAlternative}`;
 
+            alternativeEl.addEventListener('click', () => {
+                this._handleAnswer(alternativeIndex, correctAnswerIndex);
+            })
+
             this._alternativeListEl.appendChild(alternativeEl);
             currentAlternativeCharacter = CharacterUtil.getNext(currentAlternativeCharacter);
-        }
+        })
+    
     }
 
+    _handleAnswer(answerIndex, correctAnswerIndex) {
+        if(this._isAnswerCorrect(answerIndex, correctAnswerIndex)) {
+            this.multiplier += 0.25;
+        } else {
+            this.multiplier = 1;
+        }
+    } 
+
+    _isAnswerCorrect = (answerIndex, correctAnswerIndex) => answerIndex == correctAnswerIndex;
 
 }
