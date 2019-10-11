8c79f17212aede7f38c8d61f7a37856316dae577
Pedro Paulo <ppagbpedro@gmail.com>
2019-10-11 18:25:01 -0300

    Refactoring AnswerHandler to abstract way the answers is dealt with

--- js/Handlers/AnswerHandler.js
+++ js/Handlers/AnswerHandler.js
@@ -2,28 +2,38 @@ class AnswerHandler{
 
     constructor(resourceController){
         this._resourceController = resourceController;
-
+        this._answerState = false;
         this._resourceAmountPerAnswer = 10;
         this._resourceMultiplier = 1;
     }
 
-    handleCorrect(resource){
+    handleAnswer(attemptAnswerIndex, correctAnswerIndex) {
+        if(attemptAnswerIndex === correctAnswerIndex) {
+            this._handleCorrect('glassware');
+            this._answerState = true;
+        } else {
+            this._answerState = false;
+            this._handleIncorrect();
+        }
+    }
 
+    _handleCorrect(resource){
         // soundFXController.play('correct-answer');
         this._resourceMultiplier += 0.25;
         this._resourceController.increment(resource, this._resourceAmountPerAnswer * this._resourceMultiplier);
-        this._handle();
     }
 
-    handleIncorrect(resource){
-
+    _handleIncorrect(resource){
         // soundFXController.play('wrong-answer');
         this._resourceMultiplier = 1;
-        this._handle();
     }
 
+    isAnswerCorrect() {
+        return this._answerState;
+    }
 
     get resourceMultiplier(){
         return this._resourceMultiplier;
     }
-}
+
+}   
