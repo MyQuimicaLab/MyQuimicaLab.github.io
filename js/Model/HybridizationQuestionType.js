6102df625b0aec4aa8d1c0844088c9e931c76ef8
Pedro Paulo <ppagbpedro@gmail.com>
2019-10-21 20:45:04 -0300

    Shortening and simplifying statement of questions

--- js/Model/HybridizationQuestionType.js
+++ js/Model/HybridizationQuestionType.js
@@ -8,8 +8,8 @@ class HybridizationQuestionType extends QuestionType {
               alternativeMolecules = super._getAlternativeMolecules(molecules, choosenMolecule, "tipoHibridizacao");
 
         const questionDescription = `A increditável molécula ${choosenMolecule["nomeIUPAC"]} 
-            tem essa fórmula molecular aqui: ${choosenMolecule["formulaMolecular"]}. Fala que não é maneiro? 
-            Bom, a partir disso dá pra saber que a hibridização dos carbonos dela é: `,
+            tem essa fórmula molecular aqui: ${choosenMolecule["formulaMolecular"]}. Maneiro, não é? 
+            A partir disso dá pra saber que a hibridização dos carbonos dela é: `,
               questionAlternatives = ArrayUtil.shuffle([choosenMolecule, ...alternativeMolecules]).map(molecule => molecule.tipoHibridizacao),
               correctAnswerIndex = questionAlternatives.indexOf(choosenMolecule.tipoHibridizacao);
         
